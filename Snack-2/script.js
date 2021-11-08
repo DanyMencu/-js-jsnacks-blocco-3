e07f02207e6934a4d69431a48350c35c433e33a8
DanyMencu <dany69men@gmail.com>
2021-11-08 14:36:20 +0100

    Snack 2 completed

--- Snack-2/script.js
+++ Snack-2/script.js
@@ -1,2 +1,8 @@
 //*Snack 2
 //A partire da un array di numeri, genera un secondo array con le potenze al quadrato di ogni numero.
+
+const numbers = [1, 2, 3, 4, 5, 6, 7,]
+
+const squareNumbers = numbers.map(number => number * number);
+
+console.log(squareNumbers);
